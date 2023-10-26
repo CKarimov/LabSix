515ab6f89110f50ce3893918c8c44c480f634dcd
CKarimov <caleb.karimov@ufl.edu>
2023-10-25 23:17:15 -0400

    added menu

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+menu = ("Menu\n"
+		"-------------\n"
+		"1. Encode\n"
+		"2. Decode\n"
+		"3. Quit")
+
+password = ""
+
+
 def encode(number_string: str) -> str:
 	output = ""
 	for number in number_string:
@@ -8,10 +17,27 @@ def encode(number_string: str) -> str:
 			add_to_output -= 1
 		output += str(add_to_output)
 	return output
+
+
+def decode(encoded_string: str) -> str:
+	pass
+
+
 def main():
-	user_input = input("Enter your number")
-	encoded = encode(user_input)
-	print(encoded)
+
+	while True:
+		print(menu)
+		selection = input("Please enter an option: ")
+		if selection == "1":
+			original_pass = input("Please enter your password to encode:")
+			global password
+			password = encode(original_pass)
+			print("Your password has been encoded and stored!")
+		if selection == "2":
+			print(f"The encoded password is {password}, and the original password is {decode(password)}.")
+		if selection == "3":
+			exit()
+
 
 if __name__ == "__main__":
 	main()
