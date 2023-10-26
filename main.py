5eecfecb3888fb2e30bc363f4166a1eaac86d445
CKarimov <caleb.karimov@ufl.edu>
2023-10-25 22:51:03 -0400

    fixed encode

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ def encode(number_string: str) -> str:
 	output = ""
 	for number in number_string:
 		add_to_output = int(number)
-		add_to_ouput += 3
-		add_to_ouput = add_to_output % 9
+		add_to_output += 3
+		add_to_output = add_to_output % 9
 		output += str(add_to_output)
+	return output
 def main():
 	user_input = input("Enter your number")
 	encoded = encode(user_input)
 	print(encoded)
 
-if __name__ == "__main__"
+if __name__ == "__main__":
 	main()
